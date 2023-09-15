dedadb270a37bfc4bcde31536bc630926d90dea8
APAC\VADIRAJ <cognizant.c.raj@mercedes-benz.com>
2023-09-15 19:35:08 +0530

    first commit

---
+++ src/main/java/com/vadi/App.java
@@ -0,0 +1,13 @@
+package com.vadi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
